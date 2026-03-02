1bfc5c90dabc8b860c3adc46b7a09c5b6fc81dd3
pmarg <pmargaris.ac@gmail.com>
2026-03-02 11:41:31 +0000

    Deploying to gh-pages from @ pmarg/panosmargaris@671d29ef7ba1060ece5a9c696fbe23cd5be64b77 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://pmarg.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://pmarg.github.io/panosmargaris/# the subpath of your site, e.g. /blog/. Leave blank for root/#contact">contact us</a>.'
               }
             ]
           }
